b5f85a09c837bbf313c1dafad50b69a72a5b48b6
Clay Szlosek <clay.szlosek@gmail.com>
2019-01-20 23:43:52 -0500

    im dumb

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,9 +6,6 @@
 </div>
 <div class="position-ref top-links">
     <div class="content">
-        <div class="title m-b-md">
-        </div>
-
         <div class="links">
             <a href="https://www.linkedin.com/in/clay-szlosek">LinkedIn</a>
             <a href="https://www.github.com/cszlo">Github</a>
