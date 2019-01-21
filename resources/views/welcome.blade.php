7270c01a26255fd1ab950d3fc5927cd7b99d5ac5
Clay Szlosek <clay.szlosek@gmail.com>
2019-01-21 00:10:12 -0500

    im dumb

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,16 +5,14 @@
 
 </div> -->
 <div class="position-ref top-links">
-    <!-- <div class="content"> -->
-        <div class="link" style="float: left;">
-          <a href="https://www.linkedin.com/in/clay-szlosek">LinkedIn</a>
-        </div>
-        <div class="links">
-            <a href="https://www.linkedin.com/in/clay-szlosek">LinkedIn</a>
-            <a href="https://www.github.com/cszlo">Github</a>
-            <a href="https://github.com/laravel/laravel">Resume</a>
-            <a href="/blog">Blog</a>
-        </div>
-    <!-- </div> -->
+  <div class="links" style="float: left;">
+    <a href="https://www.linkedin.com/in/clay-szlosek">LinkedIn</a>
+  </div>
+  <div class="links">
+      <a href="https://www.linkedin.com/in/clay-szlosek">LinkedIn</a>
+      <a href="https://www.github.com/cszlo">Github</a>
+      <a href="https://github.com/laravel/laravel">Resume</a>
+      <a href="/blog">Blog</a>
+  </div>
 </div>
 @endsection
