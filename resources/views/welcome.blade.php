cecf20cd567bafd9ced61902d6ae8be19eb1afaa
Clay Szlosek <clay.szlosek@gmail.com>
2019-01-20 22:09:58 -0500

    maybe now...

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
 
-        <link rel="stylesheet" type="text/css" href="/Styles/styles.css" />
+        <link rel="stylesheet" type="text/css" href="./styles/styles.css" />
 
     </head>
     <body>
