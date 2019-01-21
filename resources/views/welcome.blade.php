d9116bd4f4ca7b0a16e4728e47018627e74dc072
Clay Szlosek <clay.szlosek@gmail.com>
2019-01-20 22:14:39 -0500

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,15 +9,66 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
 
-        <link rel="stylesheet" type="text/css" href="./styles/styles.css" />
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #ffa;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
 
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
     </head>
     <body>
       @yield('content')
 
 
-        <!-- <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
+        <div class="flex-center position-ref full-height">
+            <!-- @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
@@ -29,7 +80,7 @@
                         @endif
                     @endauth
                 </div>
-            @endif
+            @endif -->
 
             <div class="content">
                 <div class="title m-b-md">
@@ -40,10 +91,10 @@
                     <a href="https://www.linkedin.com/in/clay-szlosek">LinkedIn</a>
                     <a href="https://www.github.com/cszlo">Github</a>
                     <a href="https://github.com/laravel/laravel">Resume</a>
-		    <a href="/blog">Blog</a>
-		<a href="/test">Test</a>
+		                <a href="/blog">Blog</a>
+		                <a href="/test">Test</a>
                 </div>
             </div>
-        </div> -->
+        </div>
     </body>
 </html>
